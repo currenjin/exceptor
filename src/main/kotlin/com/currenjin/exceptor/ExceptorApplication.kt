1e129c6607ae6f6d3cdb93c9f134cb454d9e5531
wm-currenjin <currenjin@wemeetmobility.com>
2024-01-16 20:22:08 +0900

    Init: Project

---
+++ src/main/kotlin/com/currenjin/exceptor/ExceptorApplication.kt
@@ -0,0 +1,11 @@
+package com.currenjin.exceptor
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ExceptorApplication
+
+fun main(args: Array<String>) {
+    runApplication<ExceptorApplication>(*args)
+}
